2cb52b2dde0ee6d5f8fbff216083575abad88c54
Anirup Patnaik <anirup049@gmail.com>
2020-07-30 15:56:18 +0530

    Minor change

--- src/scenes/chat-bot-screen/view.tsx
+++ src/scenes/chat-bot-screen/view.tsx
@@ -5,21 +5,10 @@ import { CharBotScreenProps, CharBotScreenState, CharBotScreenDispatchProps } fr
 import { AppState, AppActionTypes } from '../../store';
 import { connect } from 'react-redux';
 import { getStackStyles } from '../../commons/styles/stack-style-constants';
-import { Product } from '../../components/organisms/product/view';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { RAntIconButton } from '../../components/atoms/r-ant-icon-button/view';
-import navigations from '../../navigations';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import SlidingUpPanel from 'rn-sliding-up-panel';
-import { LogoImage } from '../../components/molecules/logo-image/view';
-import { RTitleText } from '@virtuelabs-io/rapido-modules/src/components/atoms/r-title-text/view';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Entypo } from '@expo/vector-icons'; 
 
-const { height } = Dimensions.get("window");
-const _draggedValue = new Animated.Value(180);
+
+
 class ChatBotScreen extends React.Component<CharBotScreenProps, CharBotScreenState> {
-    _panel: SlidingUpPanel | null = null;
 
     constructor(props: CharBotScreenProps, state: CharBotScreenState) {
         super(props)
