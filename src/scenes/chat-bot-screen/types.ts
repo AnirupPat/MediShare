4324f3573eac3da5dad193dd282af4cb9f8a7d00
Anirup Patnaik <anirup049@gmail.com>
2020-07-30 22:46:22 +0530

    Basic setup of Bot

--- src/scenes/chat-bot-screen/types.ts
+++ src/scenes/chat-bot-screen/types.ts
@@ -4,7 +4,20 @@ import { RequestStackNavigationParamsType } from '../../store/core/types'
 
 export type CharBotScreenParamProps = StackScreenProps<RequestStackNavigationParamsType, 'chatbot'>
 
-export type CharBotScreenState = { }
+export type CharBotScreenState = { 
+    messages: [
+        {
+          _id: number,
+          text: string,
+          createdAt: any,
+          user: {
+            _id: number,
+            name: string,
+            avatar: string
+          }
+        }
+      ]
+}
 
 export type CharBotScreenDispatchProps = {
 
--- src/scenes/chat-bot-screen/view.tsx
+++ src/scenes/chat-bot-screen/view.tsx
@@ -1,36 +1,79 @@
-import React, { Dispatch } from 'react'
+import React, { Dispatch, useCallback, useState, useEffect } from 'react'
 import { ScrollView, View, Dimensions, Text, FlatList, Button, Animated } from 'react-native'
 import Styles from './styles'
 import { CharBotScreenProps, CharBotScreenState, CharBotScreenDispatchProps } from './types'
 import { AppState, AppActionTypes } from '../../store';
 import { connect } from 'react-redux';
 import { getStackStyles } from '../../commons/styles/stack-style-constants';
-import ChatBot from 'react-native-chatbot';
+// import ChatBot from 'react-native-chatbot';
+import { GiftedChat } from 'react-native-gifted-chat';
 
-const steps = [
-    {
-      id: '0',
-      message: 'Welcome to react chatbot!',
-      trigger: '1',
-    },
-    {
-      id: '1',
-      message: 'Bye!',
-      end: true,
-    },
-  ];
+
+
 class ChatBotScreen extends React.Component<CharBotScreenProps, CharBotScreenState> {
 
     constructor(props: CharBotScreenProps, state: CharBotScreenState) {
         super(props)
         this.props.navigation.setOptions(getStackStyles(this.props.route.params.title))
+        this.state = {
+            messages: [
+                {
+                    _id: 1,
+                    text: `Hi! I am the MediShare Bot. How can I help you today ?`,
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'MediShare Bot',
+                        avatar: require('../../assets/images/MediShare_logo.png')
+                    }
+                }
+            ]
+        }
     }
 
-    render(): React.ReactNode {
-        return (
-            <ChatBot steps={steps} />
-            
 
+    onSend = (messages : []) => {
+        console.log(messages)
+        console.log('---------------------------')
+        console.log(this.state.messages)
+        // this.state.messages.push(messages)
+        GiftedChat.append(this.state.messages, messages)
+
+            this.state = {
+                messages: [
+                    {
+                        _id: 1,
+                        text: `Hi! I am doing well !!`,
+                        createdAt: new Date(),
+                        user: {
+                            _id: 2,
+                            name: 'FAQ Bot',
+                            avatar: 'https://i.imgur.com/7k12EPD.png'
+                        }
+                    }  
+                ]
+            }
+            GiftedChat.append(this.state.messages, messages)
+
+
+        // setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+      }
+
+
+    render(): React.ReactNode {
+
+        return (
+            // <View style={{ flex: 1, backgroundColor: '#fff' }}>
+            //     <Text>ChatBot !</Text>
+            // </View>
+
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={(messages: any) => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
         )
     }
 }
